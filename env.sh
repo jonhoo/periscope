170d3ec160fcf86953d48ae96fb00e9cd8383d6a
David Bau <david.bau@gmail.com>
2015-11-16 00:02:17 -0500

    Resolve Theano version number conflict in favor of latest.

--- env.sh
+++ env.sh
@@ -17,7 +17,11 @@ fi
 . env/bin/activate
 # numpy isn't listed as a dependency in scipy, so we need to do it by hand
 python3 -m pip wheel numpy
-# for some reason pip is not following dependency_links to get Lasagne 0.2.dev1
+# Avoid confusion: Theano uses the same version number 0.7.0 for the
+# bleeding-edge version Lasagne needs and the older version on pypi.
+# So just force installation from github.
+python3 -m pip install --upgrade https://github.com/Theano/Theano/archive/master.zip
+# And pip is not following dependency_links to get Lasagne 0.2.dev1, so:
 python3 -m pip install --upgrade https://github.com/Lasagne/Lasagne/archive/master.zip
 # pip install -e works for everything else.
 python3 -m pip install --upgrade -e .
