a17b1b5d644aa6f0eca8cfdea48ae30f45307803
David Bau <david.bau@gmail.com>
2015-11-27 16:26:48 -0500

    Add support for dumping confusion matrix.
    Conflicts:
    	Makefile
    	main.py

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ parser.add_argument('-e', '--epoch-stop', type=int, help='stop after this many e
 parser.add_argument('-c', '--cache', type=argparse.FileType('ab+'), help='store/resume network state in/from this file', default=None)
 parser.add_argument('-p', '--plot', type=argparse.FileType('ab+'), help='plot network performance to this png file', default=None)
 parser.add_argument('-l', '--labels', type=argparse.FileType('wb+'), help='record test set predictions to this file', default=None)
+parser.add_argument('-x', '--confusion', type=argparse.FileType('wb+'), help='write confusion matrix to this file', default=None)
 parser.add_argument('-v', '--verbose', action='count')
 args = parser.parse_args()
 
@@ -127,9 +128,10 @@ test_5_acc = T.mean(lasagne.objectives.categorical_accuracy(test_prediction, tar
 # compile a second function computing the validation loss and accuracy:
 val_fn = theano.function([input_var, target_var], [test_loss, test_1_acc, test_5_acc])
 
-# and a final one for test output
+# a function for test output
 top5_pred = T.argsort(test_prediction, axis=1)[:, -5:]
 test_fn = theano.function([input_var], [top5_pred])
+debug_fn = theano.function([input_var], test_prediction)
 
 # randomly crops and horizontally flips a batch of images.
 def random_cropflip(inputs, crop=11, flip=False):
@@ -327,7 +329,11 @@ for epoch in range(start, end):
         pickle.dump(validation, args.cache)
 
     # Then we print the results for this epoch:
-    subtask("Epoch results: {:.2f}%/{:.2f}% (t5acc, v5acc)".format(
+    subtask(("Epoch results: " +
+        "{:.2f}%/{:.2f}% (t1acc, v1acc)" +
+        "{:.2f}%/{:.2f}% (t5acc, v5acc)").format(
+        training[-1][1] * 100,
+        validation[-1][1] * 100,
         training[-1][2] * 100,
         validation[-1][2] * 100,
     ))
@@ -348,4 +354,20 @@ if args.labels is not None:
         pred_out[s:s+args.batchsize, :] = test_fn(batch[0])[0]
         p.update(i)
         i = i+1
+    del pred_out
+
+if args.confusion is not None:
+    section("Debugging")
+    task("Evaluating confusion matrix on training data set")
+    pred_out = numpy.memmap(args.confusion, dtype=numpy.float32, shape=(len(X_train), cats), mode='w+')
+
+    test_batches = len(range(0, len(X_train) - args.batchsize + 1, args.batchsize))
+    p = progress(test_batches)
+
+    i = 0
+    for batch in iterate_minibatches(X_train, y_train, args.batchsize, shuffle=False):
+        s = i * args.batchsize
+        pred_out[s:s+args.batchsize, :] = debug_fn(1, center, batch[0])
+        i += 1
+        p.update(i)
     del pred_out
