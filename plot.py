2b9d869f69f3532cf5d4220b5f06bbb55d4c59d5
Jon Gjengset <jon@thesquareplanet.com>
2015-12-03 17:14:48 -0500

    Improve plotting for eps output

--- plot.py
+++ plot.py
@@ -14,15 +14,9 @@ parser.add_argument('model',
                     nargs='+',
                     help='path to .mdl to extract plot data from'
                     )
-parser.add_argument('-l',
-                    '--latex',
-                    help='output in a latex-friendly format',
-                    default=False,
-                    action='store_true'
-                    )
 parser.add_argument('-f', '--format', help='image format', default=None)
 parser.add_argument('-n', '--names', help='set experiment names', nargs="+")
-parser.add_argument('-t', '--title', help='plot title', default='Match error')
+parser.add_argument('-t', '--title', help='plot title', default=None)
 parser.add_argument('-s', '--set', help='plot only the given dataset', choices=['training', 'validation', 'all'], default='all')
 parser.add_argument('-k', '--atk', help='plot only accuracy at top-k', type=int, choices=[5, 1, 0], default=0)
 parser.add_argument('-m', '--max', help='y upper limit', type=float, default=1.0)
@@ -33,7 +27,14 @@ import matplotlib
 if args.format is not None:
     matplotlib.use('Agg') # avoid the need for X
 import seaborn as sns
-sns.set(style="ticks", color_codes=True, font_scale=1.5)
+fs = 1.0
+if not args.column:
+    fs *= 1.5
+font = 'sans-serif'
+if args.format == 'eps':
+    fs *= 0.7
+    font = 'serif'
+sns.set(style="darkgrid", color_codes=True, font_scale=fs, font=font)
 import matplotlib.pyplot as plt
 
 maxe = 0
@@ -68,15 +69,19 @@ for model in args.model:
 if not args.column:
     fig = plt.figure()
 else:
-    fig = plt.figure(figsize=(6, 8))
+    fig = plt.figure(figsize=(3.25, 4))
 ax_err = fig.gca()
 
 # plot error
-ax_err.grid(True)
 ax_err.set_xlim(1, maxe+1)
 ax_err.set_ylim(0, args.max)
 ax_err.yaxis.set_ticks(numpy.arange(0.0, args.max+0.1, 0.1))
-ax_err.set_title(args.title)
+if args.title is not None:
+    ax_err.set_title(args.title)
+
+ms = 4
+if args.column:
+    ms = 3
 
 tlegends = []
 for i in range(len(training)):
@@ -94,21 +99,21 @@ for i in range(len(training)):
     c = None
     xend = len(training[i])+1
     if args.atk != 5 and args.set != 'validation':
-        c = ax_err.plot(range(1, xend), [1-dp[1] for dp in training[i]], '--', color=c, marker='s', markersize=4)
+        c = ax_err.plot(range(1, xend), [1-dp[1] for dp in training[i]], '--', color=c, marker='s', markersize=ms)
         c = c[0].get_color()
         tlegends.append('{}, training, exact'.format(model))
     if args.atk != 1 and args.set != 'validation':
-        c = ax_err.plot(range(1, xend), [1-dp[2] for dp in training[i]], '--', color=c, marker='o', markersize=4)
+        c = ax_err.plot(range(1, xend), [1-dp[2] for dp in training[i]], '--', color=c, marker='o', markersize=ms)
         c = c[0].get_color()
         tlegends.append('{}, training, top 5'.format(model))
 
     xend = len(validation[i])+1
     if args.atk != 5 and args.set != 'training':
-        c = ax_err.plot(range(1, xend), [1-dp[1] for dp in validation[i]], '', color=c, marker='s', markersize=4)
+        c = ax_err.plot(range(1, xend), [1-dp[1] for dp in validation[i]], '', color=c, marker='s', markersize=ms)
         c = c[0].get_color()
         tlegends.append('{}, validation, exact'.format(model))
     if args.atk != 1 and args.set != 'training':
-        c = ax_err.plot(range(1, xend), [1-dp[2] for dp in validation[i]], '', color=c, marker='o', markersize=4)
+        c = ax_err.plot(range(1, xend), [1-dp[2] for dp in validation[i]], '', color=c, marker='o', markersize=ms)
         c = c[0].get_color()
         tlegends.append('{}, validation, top 5'.format(model))
 
